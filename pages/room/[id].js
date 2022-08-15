5dbf2b418cc7e843e6aa00419c1c73c41f302995
Lucy Toman <lucy@toman.me.uk>
2022-08-15 12:58:33 +0200

    renders chatroom id on page

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,11 +1,20 @@
 import Gohome from "components/Gohome"
 
 export default function Room() {
+    let chatroom
+
+    if (typeof window !== "undefined") {
+        chatroom = window.location.pathname.slice(-8)
+    }
+
     return (
         <>
             <h1 className="mt-20 text-center text-3xl uppercase font-black text-emerald-200">
-                Room
+                Welcome to your chatroom
             </h1>
+            <div className="mt-20 text-center text-2xl  text-orange-300">
+                room id: {chatroom}
+            </div>
             <Gohome />
         </>
     )
