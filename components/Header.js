5104750596e932c714126f3568695ed316f457d9
Lucy Toman <lucy@toman.me.uk>
2022-08-15 10:58:48 +0200

    initial setup and installs tailwind

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Head from "next/head"
+
+export default function Header() {
+    return (
+        <Head>
+            <title>Fwoom</title>
+            <meta name="description" content="2 person video calling" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+}
