0f5c841327354e231dfef53857b5a01f089bdd95
Lucy Toman <lucy@toman.me.uk>
2022-08-17 17:57:13 +0200

    cleans up styling

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,8 @@ export default function Header() {
                 <meta name="description" content="2 person video calling" />
                 <link rel="icon" href="/tv.ico" />
             </Head>
-            <header className="mt-4 text-emerald-200 font-black text-center text-4xl uppercase">
+            <header className="w-full py-10 px-10 text-emerald-200 font-black  text-6xl uppercase italic inline-flex items-center border-b-2 border-b-orange-300">
+                <img src="/tv.ico" alt="tv icon" className="mr-4" />
                 {router.asPath === "/" ? (
                     <h1>welcome to Fwoom</h1>
                 ) : (
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,21 @@ import Router from "next/router"
 export default function Home() {
     return (
         <>
+            <p className="mt-10 ml-10 text-orange-300 text-3xl">
+                Simple video calling for simple people:
+            </p>
+            <ol className="ml-10 text-sky-400 text-3xl">
+                <li>click on "create new chat"</li>
+                <li>share the link with a friend</li>
+                <li>chat</li>
+            </ol>
             <button
-                className=" block mx-auto text-orange-300 bg-red-800 hover:text-red-800 hover:bg-orange-300 p-3 rounded-2xl mt-20 text-2xl "
+                className=" block mx-auto text-orange-300 bg-red-800 hover:text-red-800 hover:bg-orange-300 p-3 rounded-2xl mt-20 text-2xl font-bold"
                 onClick={() => {
                     Router.push(`/room/${crypto.randomUUID().split("-")[0]}`)
                 }}
             >
-                create a new chat room
+                create new chat
             </button>
         </>
     )
